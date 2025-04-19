f0d1b48eb263fb88f8655613a359a8695e91a8f2
nicoplacente <nicolasplacente@gmail.com>
2025-04-19 01:34:29 -0300

    probando theme color

---
+++ src/page-sections/footer.jsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+export default function Footer() {
+  return (
+    <footer className="bg-secondary shadow-sm">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            href="/"
+            aria-label="Inicio"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="/logo-codeluxe.webp"
+              className="h-8 rounded-full active:scale-90 hover:contrast-50 transition cursor-pointer hover:shadow-2xl hover:shadow-primary/50"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-primary-txt">
+              Codeluxe
+            </span>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm text-secondary-txt sm:mb-0">
+            <li>
+              <Link
+                href="/info"
+                className="hover:underline me-4 md:me-6"
+                aria-label="Sobre nosotros"
+              >
+                Sobre nosotros
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/terminos"
+                className="hover:underline me-4 md:me-6"
+                aria-label="Términos y condiciones"
+              >
+                Términos y condiciones
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/propuesta"
+                className="hover:underline"
+                aria-label="Solicitá una propuesta"
+              >
+                Solicitá una propuesta
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-primary/10 sm:mx-auto lg:my-8" />
+        <span className="block text-sm text-secondary-txt sm:text-center ">
+          © 2025{" "}
+          <Link href="/" className="hover:underline" aria-label="Inicio">
+            Codeluxe™
+          </Link>
+          . Todos los derechos reservados.
+        </span>
+      </div>
+    </footer>
+  );
+}
--- src/page-sections/header.jsx
+++ src/page-sections/header.jsx
@@ -1,14 +1,17 @@
 import { navOptions } from "@/utils/constants";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="z-10 sticky top-0">
       <nav className="flex mx-auto justify-between px-8 py-2 items-center text-primary-txt max-w-4xl">
-        <img
-          src="/logo-codeluxe.jpeg"
-          alt="Logo Codeluxe"
-          className="size-12 rounded-full active:scale-90 hover:contrast-50 transition duration-300 cursor-pointer hover:shadow-2xl hover:shadow-primary/50"
-        />
+        <Link href="/" aria-label="Inicio">
+          <img
+            src="/logo-codeluxe.webp"
+            alt="Logo Codeluxe"
+            className="size-12 rounded-full active:scale-90 hover:contrast-50 transition cursor-pointer hover:shadow-2xl hover:shadow-primary/50"
+          />
+        </Link>
 
         <ul className="flex gap-2">
           {navOptions.map((option) => (
@@ -16,7 +19,7 @@ export default function Header() {
               key={option.title}
               href={option.href}
               aria-label={option.title}
-              className="text-primary-txt hover:shadow-inner hover:shadow-primary/50 hover:scale-110 p-2 rounded-xl transition duration-300"
+              className="text-primary-txt hover:shadow-inner hover:shadow-primary/50 hover:scale-110 p-2 rounded-xl transition"
             >
               <option.icon />
             </a>
